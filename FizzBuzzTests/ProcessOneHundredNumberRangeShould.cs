09ee7aa20427589bc7716b264a0fc24d3e0b1f73
rossfar <118555795+rossfar@users.noreply.github.com>
2023-09-18 10:59:40 +0100

    Added in tests for the full 1 - 100 range

---
+++ FizzBuzzTests/ProcessOneHundredNumberRangeShould.cs
@@ -0,0 +1,51 @@
+﻿using FizzBuzz;
+using Xunit;
+
+namespace FizzBuzzTests
+{
+    public class ProcessOneHundredNumberRangeShould
+    {
+        private int _min = 1;
+        private int _max = 100;
+
+        [Fact]
+        public void ReturnOneHundredResults()
+        {
+            NumberCruncher numberCruncher = new NumberCruncher();
+            List<string> results = numberCruncher.CheckNumberRange(_min, _max);
+            Assert.True(results.Count == 100);
+        }
+
+        [Fact]
+        public void ReturnFourteenBuzz()
+        {
+            NumberCruncher numberCruncher = new NumberCruncher();
+            List<string> results = numberCruncher.CheckNumberRange(_min, _max);
+            Assert.True(results.Where(p => p == Constants.FIZZBUZZ).Count() == 14);
+        }
+
+        [Fact]
+        public void ReturnTwentySevenFizz()
+        {
+            NumberCruncher numberCruncher = new NumberCruncher();
+            List<string> results = numberCruncher.CheckNumberRange(_min, _max);
+            Assert.True(results.Where(p => p == Constants.BUZZ).Count() == 27);
+        }
+
+        [Fact]
+        public void ReturnSixFizzBuzz()
+        {
+            NumberCruncher numberCruncher = new NumberCruncher();
+            List<string> results = numberCruncher.CheckNumberRange(_min, _max);
+            Assert.True(results.Where(p => p == Constants.FIZZ).Count() == 6);
+        }
+
+        [Fact]
+        public void ReturnFiftyThreeNumbers()
+        {
+            NumberCruncher numberCruncher = new NumberCruncher();
+            List<string> results = numberCruncher.CheckNumberRange(_min, _max);
+            Assert.True(results.Where(p => p != Constants.FIZZ && p != Constants.BUZZ && p != Constants.FIZZBUZZ).Count() == 53);
+        }
+    }
+}
